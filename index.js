fb61e7ddf22188beba921015ee78543b7287e099
Dana Kulp <dkulp23@gmail.com>
2016-09-24 10:05:38 -0700

    Changed HTML code to index.js from app.js since that's what I named my file.

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-/*var sum = function(x, y) {
+var sum = function(x, y) {
 	var add = (x + y);
-	var string = 'The sum of ' +  x + ' and ' + y + ' is ' + add + '. ';
-	var addArray = [add,string];
+	var string = 'The sum of ' +  x + ' and ' + y + ' is ' + add + '.';
+	var addArray = [add, string];
 	return addArray;
 }
 
